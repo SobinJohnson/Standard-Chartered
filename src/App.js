0c666cd8f28509ff014db389488c13b86eafee7e
Sobin Johnson <sobinjohnson03@gmail.com>
2024-03-14 15:08:09 +0530

    Adding Chatbox

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import MainComponent from "./MainComponent";
 import bg from "./images/bg.png";
+import ChatBox from "./ChatBox";
 function App() {
   return (
-    <div className="w-screen">
-      <img src={bg} className="w-full h-screen" alt="Background"></img>
+    <div className="w-screen  flex">
+      <img src={bg} className="w-full h-screen absolute inset-0 -z-10" alt="Background"></img>
+      <div className="w-2/3"></div>
+      <div className="w/1/3">
+        <ChatBox></ChatBox>
+      </div>
+
     </div>
   );
 }
---
+++ src/ChatBox.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChatBox = () => {
+  return (
+    <div className='bg-white text-3xl'>ChatBox</div>
+  )
+}
+
+export default ChatBox
